27b776bdb7783189918049c22f26f2e7780bf1f9
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2023-11-10 10:06:04 -0500

    generated skeleton code

---
+++ src/main/java/com/acc/jobradar/JobRadarApplication.java
@@ -0,0 +1,13 @@
+package com.acc.jobradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRadarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRadarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acc/jobradar/JobRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.jobradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
